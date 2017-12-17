c52c3590587bbaa0d111f0fd3abe0822e79813c6
nikesh pathak <justniik@gmail.com>
2017-12-17 15:34:02 +0530

    initial commit

---
+++ src/main/java/com/example/websocket/demo/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.websocket.demo;
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws InterruptedException {
+       // Thread.sleep(2000);
+        return new Greeting("Hello, "+ message.getName() + "!");
+    }
+}
---
+++ src/main/java/com/example/websocket/demo/HelloMessage.java
@@ -0,0 +1,19 @@
+package com.example.websocket.demo;
+
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage()
+    {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
